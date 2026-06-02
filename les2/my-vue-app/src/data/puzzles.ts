850fbfcfa867965d0726e959eadfeebab2558923
Invictum-Fitness <info@invictum-fitness.com>
2026-06-02 16:01:55 +0200

    fix: split puzzle & timeline item

---
+++ les2/my-vue-app/src/data/puzzles.ts
@@ -0,0 +1,6 @@
+export const puzzles = [
+    {
+        id: "1", description: "Het jaar is 1983. Een mysterieuze computer stuurt je een versleuteld bericht. Raad de letters voordat de verbinding verbroken wordt!"
+    }
+
+]
