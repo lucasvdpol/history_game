bd309a3ba98a47d4786549588f82c2b8446bda9c
Lucas van de Pol <s1156206@student.hsleiden.nl>
2026-06-04 10:04:15 +0200

    feat: added WAP puzzle

---
+++ src/components/1999/WAP.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import draggable from 'vuedraggable'
+import { computed, ref } from 'vue'
+
+const puzzleItems = ref(["<card title='Nieuws'>", "<p>Klik hier</p>", "<a href='...'>", "<card title='Menu'>"])
+const correctOrder = ["<card title='Menu'>", "<card title='Nieuws'>", "<p>Klik hier</p>", "<a href='...'>"]
+const correct = computed(() => puzzleItems.value.join() === correctOrder.join())
+
+const drag = ref(false)
+
+</script>
+
+<template>
+  <div>
+    <draggable v-model="puzzleItems" group="people" item-key="name" @start="drag=true" @end="drag=false">
+    <template #item="{ element }">
+      <div>{{ element }}</div>
+    </template>
+  </draggable>
+  </div>
+  <div v-if="correct">Correct!</div>
+</template>
+
+<style></style>
